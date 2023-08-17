b1c79892fe5909d8cd3e30c080545dd62d9761f8
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2023-08-17 11:40:23 +0500

    Update main.ts

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,15 @@
+import * as crypto from "crypto";
 
+function generate(length: number = 16) : string {
+  const uppercase: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase: string = "abcdefghijklmnopqrstuvwxyz";
+  const numbers: string = "0123456789";
+  const symbols: string = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  const all: string = uppercase + lowercase + numbers + symbols;
+  let password: string = "";
+  for (let index: number = 0; index < length; index++) {
+    const randomNumber: number = crypto.randomInt(all.length);
+    password += all.charAt(randomNumber);
+  }
+  return password;
+}
